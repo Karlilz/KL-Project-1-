f6c0fae1ecc5046675afe5a288c4a105348c7f5b
Karla Lazarte <karlalazarte@stephens-mbp.myfiosgateway.com>
2023-07-05 16:32:57 -0400

    js changes

--- README.md
+++ README.md
@@ -100,14 +100,13 @@ Link to helpful resources also included!
 
 <p align="right">(<a href="#top">back to top</a>)</p>
 
-
-
 ### Built With
 
 - HTML5
 - CSS3
 - JavaScript
 - jQuery
+- Google Fonts 
 
 ### Wireframe 
 ![**Project 1 Wireframe**](images/wireframe.jpeg)
@@ -116,16 +115,6 @@ Link to helpful resources also included!
 <!-- GETTING STARTED -->
 <!-- ## Getting Started
 
-This is an example of how you may give instructions on setting up your project locally.
-To get a local copy up and running follow these simple example steps.
-
-### Prerequisites
-
-This is an example of how to list things you need to use the software and how to install them.
-* npm
-  ```sh
-  npm install npm@latest -g
-  ``` -->
 
 <!-- ### Installation
 
--- script.js
+++ script.js
@@ -1,11 +1,50 @@
-MY_API = pNkg23WjhaVFLfB5sYb6jClwyXTF3p5oUmoHPVDc
-
-URL = 
-
-
-https://fdc.nal.usda.gov/api-guide.html 
+MY_API = Oym+WyayDAA1j/HCLtt8Kw==oZmnwwtgt5FdnvFh 
 
 
 
-https://www.edamam.com/ 
+// Constants and Variables
+const API_KEY = config.MY_KEY; 
+const BASE_URL = 'https://api.api-ninjas.com/v1/nutrition?query='; 
+ 
+let weatherData, userInput;
+
+// Cached Element References 
+const food = $('#food');
+const servingSize = $('#servingSize');
+const calories = $('#calories');
+const totalFats = $('#totalFats');
+const carbohydrates = $('#carbohydrate');
+const protein = $('#protein');
+const input = $('input[type="text"]');
+const form = $('form');
+
+// Event Listeners
+$('form').on('submit', handleGetData);
+
+// Functions
+function handleGetData(event) {
+    event.preventDefault();
+    userInput = input.val();
+     $.ajax({
+      url: `https://api.api-ninjas.com/v1/nutrition?query=${userInput}&appid=${API_KEY}`
+    }).then(
+      (data) => {
+          nutritionalData = data;
+          render(nutritionalData);
+      },
+      (error) => {
+        alert("Invalid Request. Please try again")
+      }
+  );
+} 
+
+function render(nutritionalData) {
+  title.text(weatherData.name);
+  // Use Math.round to round to nearest whole number and + "°F" to get temp value in Fahrenheit
+  temperature.text(Math.round(nutritionalData.main.temp)); 
+  index.text(Math.round(weatherData.main.feels_like)); 
+  description.text(weatherData.weather[0].description);
+  input.text(weatherData.input);
+}
+
 
