c5a2066bf8a2c4a385530cc1c635e4bd43bdf23c
Karla Lazarte <karlalazarte@stephens-mbp.myfiosgateway.com>
2023-07-06 16:51:05 -0400

    html and js upgrades

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Constants and Variables
+
 const MY_API = 'Oym+WyayDAA1j/HCLtt8Kw==oZmnwwtgt5FdnvFh'; 
 const BASE_URL = 'https://api.api-ninjas.com/v1/nutrition?query='; 
  
@@ -9,7 +9,9 @@ const food = $('#food');
 const servingSize = $('#servingSize');
 const calories = $('#calories');
 const totalFats = $('#totalFats');
-const carbohydrates = $('#carbohydrate');
+const cholesterol = $('#cholesterol');
+const sodium = $('#sodium');
+const carbohydrates = $('#carbohydrates');
 const protein = $('#protein');
 const input = $('input[type="text"]');
 const form = $('form');
@@ -25,51 +27,33 @@ function handleGetData(event) {
         method: 'GET',
         url: 'https://api.api-ninjas.com/v1/nutrition?query=' + userInput,
         headers: { 'X-Api-Key': MY_API},
-        contentType: 'application/json',
-        success: function(result) {
-            render(result);
-        },
-        error: function ajaxError(jqXHR) {
-            console.error('Error: ', jqXHR.responseText);
-        }
-    });
-}
-//      $.ajax({
-//       method: 'GET',
-//       url: `https://api.api-ninjas.com/v1/nutrition?query=${userInput}`,
-//       headers: {'X-Api-Key': MY_API},
-//     }).then(
-//       (data) => {
-//           nutritionalData = data;
-//           render(nutritionalData);
-//       },
-//       (error) => {
-//         alert("Invalid Request. Please try again")
-//       }
-//   );
-// } 
-
+    }).then(
+              (data) => {
+                  nutritionalData = data;
+                  render(nutritionalData);
+              },
+              (error) => {
+                alert("Invalid Request. Please try again")
+              })
+            }
 
 //         "name": "apple",
-//         "calories": 53.0,
 //         "serving_size_g": 100.0,
+//         "calories": 53.0,
 //         "fat_total_g": 0.2,
-//         "fat_saturated_g": 0.0,
-//         "protein_g": 0.3,
-//         "sodium_mg": 1,
-//         "potassium_mg": 11,
 //         "cholesterol_mg": 0,
+//         "sodium_mg": 1,
 //         "carbohydrates_total_g": 14.1,
-//         "fiber_g": 2.4,
-//         "sugar_g": 10.3
+//         "protein_g": 0.3,
 
 function render(nutritionalData) {
-    food.text(nutritionalData.name);
-    servingSize.text(nutritionalData.serving_size_g);
-    calories.text(nutritionalData.calories);
-    totalFats.text(nutritionalData.fat_total_g);
-    carbohydrates.text(nutritionalData.carbohydrates_total_g);
-    protein.text(nutritionalData.protein_g);
-    input.val(userInput);
-    console.log(nutritionalData.name)
+    const foodData = nutritionalData[0];
+    food.text(foodData.name);
+    servingSize.text(Math.round(foodData.serving_size_g) + "g");
+    calories.text(Math.round(foodData.calories));
+    totalFats.text(Math.round(foodData.fat_total_g) + "g");
+    cholesterol.text(Math.round(foodData.cholesterol_mg) + "mg");
+    sodium.text(Math.round(foodData.sodium_mg) + "mg");
+    carbohydrates.text(Math.round(foodData.carbohydrates_total_g) + "g");
+    protein.text(Math.round(foodData.protein_g) + "g");
 }
